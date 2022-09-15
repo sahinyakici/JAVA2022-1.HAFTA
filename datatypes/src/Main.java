74de787b42d4be8ea11ddd0628574cd0fd030dba
Şahin Yakıcı <shnykc@hotmail.com>
2022-09-15 17:29:31 +0300

    İzlenen videolar
    1-2-3-4-5-6-7

---
+++ datatypes/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double sayi = 128;
+        sayi = -129;
+        String şehir = "Ankara";
+        char harf = 't';
+        boolean bool = true;
+
+    }
+}
---
+++ variables/src/variables/Main.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 10;
+		String mesaj="Öğrenci sayim : ";
+		
+		System.out.println("Öğrenci sayim : 10");
+		System.out.println("Öğenci sayim : "+ogrenciSayisi);
+		System.out.println(mesaj+ogrenciSayisi);
+
+	}
+
+}
